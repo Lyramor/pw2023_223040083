869d112b65c6462055a04d590ef17ee078033102
Lyramor <mmarsa2435@gmail.com>
2023-02-16 17:50:03 +0700

    mengerjakan tugas 1

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,24 @@
+<?php 
+    $a = 1;
+    $nama = "Muhamad Marsa Nur Jaman";
+    $nrp = "223040083";
+    $mk = "Pemrograman WEB"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 2</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Hello $nama" . "</br>";
+        echo "Hello $mk"; 
+        ?>
+    </h1>
+</body>
+</html>
